35405effb6e23cbb0937eb393da5bc8088fa3068
Igor Becker <igor_nyu@hotmail.com>
2024-05-07 20:49:56 -0300

    feat: adding logic to c implementation

--- c/build.sh
+++ c/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 gcc -I./raylib/include -o game_of_life main.c -L./raylib/lib -l:libraylib.a -lm
+
+./game_of_life
--- c/main.c
+++ c/main.c
@@ -1,13 +1,14 @@
+#include <stdio.h>
 #include <stdlib.h>
-
+#include <time.h>
 #include <raylib.h>
 
-#define WIDTH      800
-#define HEIGHT     600
+#define WIDTH      5
+#define HEIGHT     5
 #define CELL_SIZE  5
 #define LIVE       1
 #define DEAD       0
-#define TITLE "Game of Life"
+#define TITLE      "Game of Life"
 
 void create_grid(int (*grid)[WIDTH]) {
     srand(time(NULL));
@@ -17,7 +18,6 @@ void create_grid(int (*grid)[WIDTH]) {
             grid[i][j] = r;
         }
     }
-    return;
 }
 
 void draw_cells(int (*grid)[WIDTH]) {
@@ -33,9 +33,33 @@ void draw_cells(int (*grid)[WIDTH]) {
     }
 }
 
-int is_cell_alive(int cell, int (*grid)[WIDTH]) {
-    int nbors = calculate_nbors(grid);
-    switch(nbors) {
+int calculate_nbors(int (*grid)[WIDTH], int row, int col) {
+    int neighbors[8][2] = {
+        {-1, -1}, {-1, 0}, {-1, 1},
+        {0, -1},           {0, 1},
+        {1, -1}, {1, 0}, {1, 1}
+    };
+    int aliveCells = 0;
+    for (int i = 0; i < 8; i++) {
+        int newRow = row + neighbors[i][0];
+        int newCol = col + neighbors[i][1];
+
+        if (newRow >= 0 && newRow < HEIGHT && newCol >= 0 && newCol < WIDTH) {
+            if (grid[newRow][newCol] == LIVE) {
+                aliveCells += grid[row][col];
+            }
+        }
+    }
+    return aliveCells;
+}
+
+int is_cell_alive(int (*grid)[WIDTH], int row, int col) {
+    int cell = grid[row][col];
+    int nbors = calculate_nbors(grid, row, col);
+    printf("cell: %i, nbors: %i", cell,nbors);
+    printf("\n");
+
+    switch(cell) {
         case LIVE:
             if (nbors < 2 || nbors > 3) {
                 return DEAD;
@@ -46,58 +70,50 @@ int is_cell_alive(int cell, int (*grid)[WIDTH]) {
                 return LIVE;
             }
             return DEAD;
+        default:
+            return DEAD;
     }
-}
-
-int calculate_nbors(int (*grid)[WIDTH]) {
-    int ROWS = 8;
-	int aliveCells = 0;
-	int neighbors[][2] = {
-		{-1, -1}, 
-        {-1, 0}, 
-        {-1, 1},
-		{0, -1}, 
-        {0, 1},
-		{1, -1}, 
-        {1, 0}, 
-        {1, 1},
-	};
-    for (int row = 0; row < ROWS; row++) {
-        int newRow = row + neighbors[row][0];
-        int newCol = row + neighbors[row][1];
-        if (newRow >= 0 && newRow < HEIGHT && newCol >= 0 && newCol < WIDTH) {
-            aliveCells += grid[newRow][newCol];
-        }
-    }
-	return aliveCells;
 }
 
 void generate_new_grid(int (*grid)[WIDTH]) {
+    for (int row = 0; row < HEIGHT; row++) {
+        for (int col = 0; col < WIDTH; col++) {
+            int current_cell = is_cell_alive(grid, row, col);
+            grid[row][col] = current_cell;
+        }
+    }
+}
+
+void update_gol(int (*grid)[HEIGHT]) {
+    draw_cells(grid);
+    generate_new_grid(grid);
+}
+
+void printGrid(int grid[][WIDTH]) {
     for (int i = 0; i < HEIGHT; i++) {
         for (int j = 0; j < WIDTH; j++) {
-            int current_cell = is_cell_alive(grid[i][j], grid);
-            grid[i][j] = current_cell;
+            printf("%d ", grid[i][j]);
         }
+        printf("\n");
     }
 }
 
 int main(void) {
-    InitWindow(WIDTH, HEIGHT, TITLE);
+    // InitWindow(WIDTH, HEIGHT, TITLE);
+    // SetTargetFPS(60);
 
     int grid[HEIGHT][WIDTH] = {0};
     create_grid(grid);
+    printGrid(grid);
+    generate_new_grid(grid);
+    printGrid(grid);
 
-    while(!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(BLACK);
-
-        draw_cells(grid);
-        generate_new_grid(grid);
-
-        EndDrawing();
-    }
-
-    CloseWindow();
+    // while(!WindowShouldClose()) {
+    //     BeginDrawing();
+    //     ClearBackground(BLACK);
+    //     EndDrawing();
+    // }
+    // CloseWindow();
 
     return 0;
 }
